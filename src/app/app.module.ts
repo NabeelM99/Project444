7f8223120e95dc413d9dfcb2052c0bfb49810209
Tex <techcenter2001@gmail.com>
2023-12-26 02:15:10 +0300

    add nacessary files to start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCb323BI8X8CTVSUXlo0IiGmjUGYgim9d0",
+  authDomain: "itcs444-projectfinalhallresrev.firebaseapp.com",
+  projectId: "itcs444-projectfinalhallresrev",
+  storageBucket: "itcs444-projectfinalhallresrev.appspot.com",
+  messagingSenderId: "128091415984",
+  appId: "1:128091415984:web:93c4e0c9cb00c45dfe6275",
+  measurementId: "G-7S21G3KCZY"
+};
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
