1c18a9dc345d4ef6f2781425cf97d7b3897bbd41
Tex <techcenter2001@gmail.com>
2023-12-28 15:11:33 +0300

    create event page complete it remain crateEvent function

--- src/app/pages/create-event/create-event.module.ts
+++ src/app/pages/create-event/create-event.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { CreateEventPageRoutingModule } from './create-event-routing.module';
@@ -13,8 +13,9 @@ import { CreateEventPage } from './create-event.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CreateEventPageRoutingModule
+    CreateEventPageRoutingModule,
+    ReactiveFormsModule,
   ],
-  declarations: [CreateEventPage]
+  declarations: [CreateEventPage],
 })
 export class CreateEventPageModule {}
--- src/app/pages/create-event/create-event.page.ts
+++ src/app/pages/create-event/create-event.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import {
+  UserManagementService,
+  reservation,
+} from 'src/app/services/user-management.service';
 
 @Component({
   selector: 'app-create-event',
@@ -6,10 +12,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-event.page.scss'],
 })
 export class CreateEventPage implements OnInit {
+  reservationID!: string;
+  reservation: reservation = {
+    id: '',
+    clientID: '',
+    start_date: '',
+    end_date: '',
+    eventID: '',
+    hallID: '',
+    start_time: '',
+    end_time: '',
+    total: 0,
+    clientName: '',
+    eventName: '',
+  };
 
-  constructor() { }
+  eventForm!: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    public formBuilder: FormBuilder,
+    public route: ActivatedRoute,
+    public userManagmentService: UserManagementService
+  ) {
+    this.eventForm = this.formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-zA-Z ]+$/),
+          Validators.minLength(4),
+          Validators.maxLength(30),
+        ],
+      ],
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.compose([
+            Validators.maxLength(255),
+            Validators.pattern(/^[a-zA-Z0-9\s\-:,.]+$/),
+          ]),
+        ],
+      ],
+      speakers: [
+        '',
+        [
+          Validators.required,
+          Validators.compose([
+            Validators.minLength(7),
+            Validators.maxLength(50),
+            Validators.pattern(/^[a-zA-Z0-9./]+$/),
+          ]),
+        ],
+      ],
+      floorPlan: [
+        '',
+        [Validators.required, Validators.pattern(/^[a-zA-Z0-9./]+$/)],
+      ],
+      agenda: [
+        '',
+        [Validators.required, Validators.pattern(/^[a-zA-Z0-9\s\-:,.]+$/)],
+      ],
+    });
   }
 
+  ngOnInit(): void {
+    this.reservationID = this.route.snapshot.paramMap.get('id') as string;
+    this.userManagmentService
+      .getReservationByID(this.reservationID)
+      .then((reservation) => {
+        if (reservation.exists()) {
+          this.reservation = reservation.data() as reservation;
+          console.log(this.reservation);
+        }
+      });
+  }
+
+  createEvent(value: any) {
+    // Implement your logic for creating an event here
+  }
 }
